ede8eeaf3c603d385eeef11b48aaa121a5e736d0
Ian Macalinao <ian@giza.us>
2014-09-13 09:45:56 -0500

    Add app stuff

--- app.js
+++ app.js
@@ -1,2 +1,10 @@
 var express = require('express');
 var mongoose = require('mongoose');
+
+var app = express();
+
+app.route('/').get(function(req, res) {
+  res.send('No home page.');
+});
+
+app.listen(process.env.PORT || 3000);
