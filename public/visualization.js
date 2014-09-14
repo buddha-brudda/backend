76a3d4fc1aaddc6919b667d511511256606cfda7
Ian Macalinao <ian@giza.us>
2014-09-14 00:44:51 -0500

    Update visualization

--- public/visualization.js
+++ public/visualization.js
@@ -1,4 +1,5 @@
 var socket = io.connect('http://www.buddhabrudda.com/');
 socket.on('cbcalled', function(data) {
+  document.getElementById('message').innerHTML = 'Received cb from ' + data.query.action;
   console.log(data);
 });
