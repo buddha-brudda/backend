6f7e307ee0f4a4bee3fd3baafb665da85496d9eb
Ian Macalinao <ian@giza.us>
2014-09-14 01:41:03 -0500

    Add vote visualization

--- public/visualization.js
+++ public/visualization.js
@@ -1,4 +1,14 @@
 var socket = io.connect('http://www.buddhabrudda.com/');
 socket.on('cbcalled', function(data) {
   $('#message').prepend('<p>Recieved ' + data.callbackId + ' from ' + data.query.name + ' with action ' + data.query.action + '.');
+  if (data.query.callbackId === 'VOTE') {
+    var votesUp = parseInt($('#votesUp').text());
+    var votesDown = parseInt($('#votesDown').text());
+
+    if (data.query.action === 'Up') {
+      $('#votesUp').html(votesUp + 1);
+    } else {
+      $('#votesDown').html(votesDown + 1);
+    }
+  }
 });
