9459da1b3c6dfe4a5e3193c9e57e15b0eeb6eb9e
Ian Macalinao <ian@giza.us>
2014-09-13 19:49:15 -0500

    Add allow headers

--- app.js
+++ app.js
@@ -15,6 +15,7 @@ var Notification = mongoose.model('Notification', new mongoose.Schema({
 var app = express();
 app.use(function(req, res, next) {
   res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'X-Requested-With');
   next();
 });
 app.use(bodyParser.json());
