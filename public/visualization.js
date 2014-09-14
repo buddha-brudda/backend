b51473818ebc4eebb5fdee51af92c4e6d38fdf1e
Ian Macalinao <ian@giza.us>
2014-09-14 00:52:06 -0500

    Add prepend message

--- public/visualization.js
+++ public/visualization.js
@@ -1,5 +1,4 @@
 var socket = io.connect('http://www.buddhabrudda.com/');
 socket.on('cbcalled', function(data) {
-  document.getElementById('message').innerHTML = 'Received cb from ' + data.query.action;
-  console.log(data);
+  $('#message').prepend('<p>Recieved ' + data.callbackId + ' from ' + data.query.name + ' with action ' + data.query.action + '.');
 });
