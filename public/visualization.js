71dce39b5f6703b4d6457d3804b2b8ddde8fcb77
Ian Macalinao <ian@giza.us>
2014-09-14 01:52:46 -0500

    bug fix

--- public/visualization.js
+++ public/visualization.js
@@ -1,9 +1,9 @@
 var socket = io.connect('http://www.buddhabrudda.com/');
 socket.on('cbcalled', function(data) {
   $('#message').prepend('<p>Recieved ' + data.callbackId + ' from ' + data.query.name + ' with action ' + data.query.action + '.');
-  if (data.query.callbackId === 'VOTE') {
-    var votesUp = parseInt($('#votesUp').text());
-    var votesDown = parseInt($('#votesDown').text());
+  if (data.callbackId === 'VOTE') {
+    var votesUp = parseInt($('#votesUp').html());
+    var votesDown = parseInt($('#votesDown').html());
 
     if (data.query.action === 'Up') {
       $('#votesUp').html(votesUp + 1);
