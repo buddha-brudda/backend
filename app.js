c6f08475250f408c96b50eee4a9d8f48c25069f7
Ian Macalinao <ian@giza.us>
2014-09-14 06:26:32 -0500

    fix header

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ var io = require('socket.io').listen(server);
 
 app.use(function(req, res, next) {
   res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Headers', 'X-Requested-With');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   next();
 });
 app.use(bodyParser.json());
